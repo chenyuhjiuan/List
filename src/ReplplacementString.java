0b963dab123cdce926e72e5b688917a834d66c83
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-09-12 11:46:05 -0400

    Intermediate Eliza

---
+++ src/ReplplacementString.java
@@ -0,0 +1,18 @@
+public class ReplplacementString {
+
+
+
+        public static void main(String[] arg)
+        {
+            String originalString = "how to do in java - java tutotials";
+
+            String newString = originalString.replace("java", "JAVA");
+
+            System.out.println(originalString);
+            System.out.println(newString);
+        }
+
+
+
+
+}
